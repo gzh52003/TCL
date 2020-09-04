69aab65079d8d8b3e1e33a79e9d44ec8db41eb82
敏捷 <13620321890@163.com>
2020-09-04 14:34:44 +0800

    合并前分支

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,49 +1,70 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
     <van-tabbar route v-show="showMenu">
-      <van-tabbar-item :icon="item.icon" v-for="item in menu" :key="item.name" :to="item.path">{{item.text}}</van-tabbar-item>
-  </van-tabbar>
+      <van-tabbar-item
+        v-for="item in menu"
+        :icon="item.icon"
+        :key="item.name"
+        :to="item.path"
+        @click="changeTab(item)"
+      >{{item.text}}</van-tabbar-item>
+    </van-tabbar>
+    <van-cell is-link @click="showPopup"></van-cell>
+    <van-popup v-model="show">内容</van-popup>
   </div>
 </template>
 <script>
-import Vue from 'vue'
-import {Button,Tabbar, TabbarItem,Tag} from 'vant';
-Vue.use(Button)
+import Vue from "vue";
+import { Button, Tabbar, TabbarItem, Tag, Popup } from "vant";
+Vue.use(Button);
 Vue.use(Tabbar);
 Vue.use(TabbarItem);
 Vue.use(Tag);
+Vue.use(Popup);
 export default {
-  data(){
+  data() {
     return {
-      showMenu:true,
-      menu:[{
-        name:'home',
-        path:'/home',
-        icon:'wap-home-o',
-        text:'首页'
+      showMenu: true,
+      show:false,
+      menu: [
+        {
+          name: "home",
+          path: "/home",
+          icon: "wap-home-o",
+          text: "首页",
+        },
+        {
+          name: "discover",
+          path: "/discover",
+          icon: "eye-o",
+          text: "发现",
+        },
+        {
+          name: "cart",
+          path: "/cart",
+          icon: "shopping-cart-o",
+          text: "购物车",
+        },
+        {
+          name: "mine",
+          path: "/mine",
+          icon: "user-o",
+          text: "我的",
+        },
+      ],
+      methods: {
+        changeTab(item){
+          console.log(item);
+        },
+        showPopup(){
+          this.show=true
+        }
       },
-      {
-        name:'discover',
-        path:'/discover',
-        icon:'eye-o',
-        text:'发现'
-      },{
-        name:'cart',
-        path:'/cart',
-        icon:'shopping-cart-o',
-        text:'购物车'
-      },{
-        name:'mine',
-        path:'/mine',
-        icon:'user-o',
-        text:'我的'
-      }]
-    }
-  }
-}
+    };
+  },
+};
 </script>
 <style>
-
 </style>
 
