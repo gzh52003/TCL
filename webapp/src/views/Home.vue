fda6095b8ebe9316835f613d92ac59752adc05ca
zhongdifa <454867353@qq.com>
2020-09-05 11:02:34 +0800

    首页

--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -29,39 +29,55 @@
     </van-grid>
     <!-- 大图 -->
     <div class="warp-1">
-      <img src="../../public/img/worp.jpg" alt="" />
+      <img src="../../public/img/worp.jpg" alt />
     </div>
 
     <ul class="warp-2 active">
       <li>
-        <a href=""><img src="../../public/img/worp1.jpg" alt=""/></a>
+        <a href>
+          <img src="../../public/img/worp1.jpg" alt />
+        </a>
       </li>
       <li>
-        <a href=""><img src="../../public/img/worp2.jpg" alt=""/></a>
+        <a href>
+          <img src="../../public/img/worp2.jpg" alt />
+        </a>
       </li>
     </ul>
     <ul class="warp-2 active">
       <li>
-        <a href=""><img src="../../public/img/worp3.jpg" alt=""/></a>
+        <a href>
+          <img src="../../public/img/worp3.jpg" alt />
+        </a>
       </li>
       <li>
-        <a href=""><img src="../../public/img/worp4.jpg" alt=""/></a>
+        <a href>
+          <img src="../../public/img/worp4.jpg" alt />
+        </a>
       </li>
     </ul>
     <ul class="warp-3 active">
       <li>
-        <a href=""><img src="../../public/img/worp5.jpg" alt=""/></a>
+        <a href>
+          <img src="../../public/img/worp5.jpg" alt />
+        </a>
       </li>
       <li>
-        <a href=""><img src="../../public/img/worp6.jpg" alt=""/></a>
+        <a href>
+          <img src="../../public/img/worp6.jpg" alt />
+        </a>
       </li>
     </ul>
     <ul class="warp-4">
       <li>
-        <a href=""><img src="../../public/img/worp7.jpg" alt=""/></a>
+        <a href>
+          <img src="../../public/img/worp7.jpg" alt />
+        </a>
       </li>
       <li>
-        <a href=""><img src="../../public/img/worp8.jpg" alt=""/></a>
+        <a href>
+          <img src="../../public/img/worp8.jpg" alt />
+        </a>
       </li>
     </ul>
     <!-- 商品轮播图 -->
@@ -69,20 +85,96 @@
     <van-swipe indicator-color="#1E90FF" class="goodsswipe">
       <van-swipe-item class="con-left con">
         <li v-for="item in goodsswipe" :key="item._id">
-          <img :src="item.cardImgSrc" alt="" />
+          <img :src="item.cardImgSrc" alt />
           <p>{{ item.title }}</p>
           <span>{{ item.price }}</span>
         </li>
       </van-swipe-item>
       <van-swipe-item class="con-right con">
         <li v-for="item in goodsright" :key="item._id">
-          <img :src="item.cardImgSrc" alt="" />
+          <img :src="item.cardImgSrc" alt />
           <p>{{ item.title }}</p>
           <span>{{ item.price }}</span>
         </li>
       </van-swipe-item>
     </van-swipe>
-    <div class="tcl-dynamic"></div>
+    <div class="box">
+      <div class="tcl-dynamic">
+        <h4>TCL动态</h4>
+        <div class="video">
+          <img src="../../public/img/card.jpg" alt />
+          <img src="../../public/img/bo.png" class="bo" alt />
+          <p>驰援武汉，共克时艰，TCL在行动！</p>
+        </div>
+        <ul class="dynamic-title">
+          <li>
+            <img src="../../public/img/9.jpg" alt />
+            <p>
+              <span>
+                TCL2020全球新品
+                <br />发布会
+              </span>
+            </p>
+          </li>
+          <li>
+            <img src="../../public/img/8.jpg" alt />
+            <p>
+              <span>
+                TCL通讯实力亮相
+                <br />2020，首款5G手机惊艳登场
+              </span>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="tcl-statement">
+      <dl>
+        <dt>
+          <img src="../../public/img/qualityproduct.png" alt />
+        </dt>
+        <dd>官方正品</dd>
+      </dl>
+      <dl>
+        <dt>
+          <img src="../../public/img/genius.png" alt />
+        </dt>
+        <dd>全国联保</dd>
+      </dl>
+      <dl>
+        <dt>
+          <img src="../../public/img/sevenday.png" alt />
+        </dt>
+        <dd>七天五理由</dd>
+      </dl>
+    </div>
+    <div class="tcl-contact">
+      <div>24小时全国热线</div>
+      <p>4008-123456</p>
+    </div>
+    <div class="tcl-others" style="padding-bottom:3rem;">
+      <div class="others-items">
+        <a href>
+          <span class="others-item">关于TCL</span>
+        </a>
+        <a hre>
+          <span class="others-item">授权体验店</span>
+        </a>
+        <a href>
+          <span class="others-item">合作加盟</span>
+        </a>
+      </div>
+      <div class="tcl-copyright">TCL集团股份有限公司版权所有</div>
+    </div>
+    <!-- 返回顶部 -->
+    <nav class="kf-up">
+      <p>
+        <van-icon name="phone-circle-o" color="red" class="icon" />
+      </p>
+      <p class="top">
+        <van-icon name="back-top" class="icon" />
+      </p>
+    </nav>
   </div>
 </template>
 
@@ -98,6 +190,7 @@ import {
   Search,
   Tab,
   Tabs,
+  Icon,
 } from "vant";
 Vue.use(Swipe);
 Vue.use(SwipeItem);
@@ -108,6 +201,7 @@ Vue.use(Image);
 Vue.use(Search);
 Vue.use(Tab);
 Vue.use(Tabs);
+Vue.use(Icon);
 
 export default {
   name: "Home",
@@ -160,12 +254,12 @@ export default {
 </script>
 
 <style lang="scss">
-html,
 body {
-  height: 100%;
+  background: #f5f5f5;
 }
-.home {
-  height: 100%;
+* {
+  margin: 0;
+  padding: 0;
 }
 .my-swipe .van-swipe-item {
   color: #fff;
@@ -238,10 +332,11 @@ body {
   margin-top: 40px;
   width: 100%;
   height: 232px;
+  background: #fff;
 
   .con {
     display: flex;
-    justify-content: space-around;
+    justify-content: space-evenly;
   }
   li {
     width: 33%;
@@ -266,9 +361,152 @@ body {
   }
 }
 // tcl-dynamic
-.tcl-dynamic {
+.box {
+  display: flex;
+  justify-content: center;
+  background: #fff;
+  .tcl-dynamic {
+    width: 345.18px;
+    height: 418px;
+
+    h4 {
+      text-align: center;
+      height: 50px;
+      line-height: 50px;
+      font-size: 24px;
+    }
+    .video {
+      height: 150px;
+      position: relative;
+
+      .bo {
+        position: absolute;
+        width: 3.048rem;
+        height: 3.048rem;
+        left: 50%;
+        top: 50%;
+        margin-top: -1.024rem;
+        margin-left: -1.024rem;
+      }
+      p {
+        text-align: center;
+        height: 40px;
+        background: #f5f5f5;
+        line-height: 40px;
+      }
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .dynamic-title {
+      margin-top: 60px;
+      display: flex;
+      justify-content: space-between;
+      height: 262px;
+      background: #fff;
+      li {
+        height: 162px;
+        width: 165px;
+        img {
+          width: 100%;
+          height: 92px;
+        }
+        p {
+          text-align: center;
+          background: #f5f5f5;
+
+          span {
+            font-size: 14px;
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 2;
+            overflow: hidden;
+          }
+        }
+      }
+    }
+  }
+}
+.tcl-statement {
+  height: 128px;
+  display: flex;
+  justify-content: space-around;
+  background: #fff;
+  margin-top: 20px;
+  dl {
+    width: 60px;
+    height: 58px;
+    text-align: center;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    dd {
+      font-size: 12px;
+    }
+  }
+}
+.tcl-contact {
+  height: 83px;
+  text-align: center;
+  background: #fff;
+  line-height: 34px;
+  div {
+    font-size: 16px;
+    margin-top: 0.4rem;
+    color: #333;
+  }
+  p {
+    font-size: 24px;
+    color: red;
+    font-weight: 700;
+    margin-top: 0.2rem;
+  }
+}
+.tcl-others {
   width: 100%;
-  height: 418px;
-  background: red;
+  height: 69.88px;
+  margin-bottom: 100px;
+  .others-items {
+    align-items: center;
+    display: flex;
+    height: 3.98rem;
+    justify-content: space-around;
+  }
+
+  a {
+    width: 75px;
+    height: 35px;
+    font-size: 14px;
+    color: #999;
+    border: 1px solid #ccc;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+
+    border-radius: 2px;
+  }
+  .tcl-copyright {
+    font-size: 14px;
+    color: #999;
+    text-align: center;
+  }
+}
+// 返回顶部
+.kf-up {
+  position: fixed;
+  bottom: 110px;
+  right: 0;
+  p {
+    background: #fff;
+    border-radius: 50%;
+  }
+  .icon {
+    font-size: 34px;
+  }
+  .top {
+    margin-top: 10px;
+  }
 }
 </style>
